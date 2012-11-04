5fdf53175fa51e62cb0aa2fb42c43e15a8c35747
Phuc Ly <lytienphuc0810@gmail.com>
2012-11-04 15:47:12 +0700

    first commit\

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TutorNetwork::Application.config.secret_token = 'ddd580aea378e3fde62a1ab0a843430ca2c7252e34dd616680388ac021cfcb1545e7aad8a526d9cd98891d35ad833daaf23f3a9f28183e9bd76f331356e68882'
