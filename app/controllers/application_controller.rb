7aa815abc5165654ffa521cbf676f8722d23a06e
Phuc Ly <lytienphuc0810@gmail.com>
2012-11-05 23:20:10 +0700

    setup layout and devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+	def after_sign_in_path_for(user)
+		case user.role
+			when "admin"
+				"/users/index/1"
+			when "user"
+				"/tutor_posts/index/1"
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 TutorNetwork::Application.routes.draw do
-  devise_for :users
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +48,11 @@ TutorNetwork::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  devise_for :users do
+    get "/", :to => "devise/sessions#new"
+  end
+
+  root :to => "devise/sessions#new"
 
   # See how all your routes lay out with "rake routes"
 
