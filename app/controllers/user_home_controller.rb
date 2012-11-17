2ef67f135a83f1867f3ac10d6b418be5b1e11df0
unknown <Administrator@AnhVu.(none)>
2012-11-17 10:57:36 +0700

    (doing) User page show delete index

---
+++ app/controllers/user_home_controller.rb
@@ -0,0 +1,17 @@
+class UserHomeController < ApplicationController
+	def index_tutor_post
+		#get method
+	end
+
+	def show_tutor_post
+		#get method
+	end
+
+	def create_recipe
+		#post method
+	end
+
+	def delete_recipe
+		#post method
+	end
+end
---
+++ test/functional/user_post_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UserPostControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
