14432dab6b9cc70ad64eb60dedeb36cb793f80fc
lytienphuc0810 <lytienphuc0810@gmail.com>
2012-11-18 22:11:59 +0700

    remove has_one user in recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,6 @@
 class Recipe < ActiveRecord::Base
   attr_accessible :poster_confirmation, :tutor_id, :customer_id
 
-  has_one :user
   belongs_to :tutor, :class_name => 'User', :foreign_key => :tutor_id
   belongs_to :customer, :class_name => 'User', :foreign_key => :customer_id
 
