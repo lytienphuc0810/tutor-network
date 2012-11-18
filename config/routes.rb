1e60d5ff863be1a6ba2cec1946413c06924bed53
Vinh Truong Nguyen <vinh@vinh-Aspire-5750G.(none)>
2012-11-19 01:33:39 +0700

    Chinh mot so logic trong view thanh title

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,7 @@ TutorNetwork::Application.routes.draw do
   post "customer_posts/update_customer_post/:customer_post_id" => "customer_posts#update_customer_post"
   get "customer_posts/index_recipe/:page" => "customer_posts#index_recipe"
   get "customer_posts/show_customer_post/:customer_post_id" => "customer_posts#show_customer_post"
-  # get "customer_homes/create_recipe/:tutor_id" => "customer_homes#create_recipe"
-  get "customer_homes/show_recipe/:recipe_id" => "customer_homes#show_recipe"
-  # get "customer_homes/delete_recipe/:recipe_id" => "customer_homes#delete_recipe"
+  get "customer_posts/show_recipe/:recipe_id" => "customer_posts#show_recipe"
 
   # get "customer_homes_controller/create_recipe" => ""
 
