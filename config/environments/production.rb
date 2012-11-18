43325157542fafa1ca3f606848a94a5b93b555a5
Phuc Ly <lytienphuc0810@gmail.com>
2012-11-19 02:16:15 +0700

    config mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,19 @@ TutorNetwork::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'tutor-network.herokuapp.com' }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: '587',
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "buffalokids009@gmail.com",
+    password: "tretrau123"
+  }
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
