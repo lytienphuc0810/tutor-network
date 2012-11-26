e0e185a55b6a45e492b9dec1ec987b2fefc340c0
lytienphuc0810 <lytienphuc0810@gmail.com>
2012-11-26 16:36:32 +0700

    fix css in post

--- app/controllers/etcs_controller.rb
+++ app/controllers/etcs_controller.rb
@@ -1,30 +1,9 @@
 class EtcsController < ApplicationController
-	before_filter :authenticate_user!
 	def faq 
 		
 	end
 
 	def contact
 		
-	end
-
-	def set_en
-		if current_user.nil?
-			session[:lang] = "en"
-		else	
-			current_user.locale = "en"
-			current_user.save
-		end
-		redirect_to "/"
-	end
-
-	def set_vi
-		if current_user.nil?
-			session[:lang] = "vi"
-		else	
-			current_user.locale = "vi"
-			current_user.save
-		end
-		redirect_to "/"
 	end
 end
